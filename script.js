016a7de0e36a438918fa4feeaeb39f96ede15321
pranshu <pranshugautam1998@gmail.com>
2023-02-11 18:45:19 +0530

    functions completed

--- script.js
+++ script.js
@@ -7,21 +7,44 @@ let arr = [
 ];
 
 function PrintStudentswithMap() {
-  //Write your code here , just console.log
+arr.map(function(value)
+{
+  if(value.marks > 50) console.log(value);
+})//Write your code here , just console.log
 }
 
 function PrintStudentsbyForEach() {
-  //Write your code here , just console.log
+ arr.forEach(function(value)
+ {
+   if(value.marks > 50) console.log(value);
+ }) //Write your code here , just console.log
 }
 
 function addData() {
-  //Write your code here, just console.log
+ var student = new Object(); //{}
+ student = {id:4,name:"susan",age:"20",marks:45}; 
+ arr.push(student);
+ console.log(arr[3]);//Write your code here, just console.log
 }
 
 function removeFailedStudent() {
-  //Write your code here, just console.log
+  for(var i = 0; i< arr.length; i++)
+  {
+      if(arr[i].marks < 50) 
+      {
+        arr.splice(i , 1);
+        i--;
+      }
+  }
+  console.log(arr);//Write your code here, just console.log
 }
 
 function concatenateArray() {
-  //Write your code here, just console.log
+  let temp = [
+    { id: 5, name: "pranshu", age: "24", marks: 100 },
+    { id: 6, name: "jagan", age: "20", marks: 25 },
+    { id: 7, name: "karina", age: "19", marks: 35 },
+  ];
+  const newArr = arr.concat(temp);
+  console.log(newArr);//Write your code here, just console.log
 }
